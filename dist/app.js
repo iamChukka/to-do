fd8b311184946ae5ea047fbdec7c2c885c4f4546
Chukwuka Eluchie <eluchiechukwuka@yahoo.co.uk>
2022-04-29 02:49:04 +0100

    Moved to typescript many errors

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 ///////////////////////////////////////////
 //app.js
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config;
+    require("dotenv").config;
 }
 const createError = require("http-errors");
-const express = require("express");
+//const express = require("express");
+const express_1 = __importDefault(require("express"));
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const initialisePassport = require("./config/passport-config");
-
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const flash = require("express-flash");
@@ -23,64 +28,48 @@ const config = require("./config/Config");
 const todos = require("./routes/Todos");
 const users = require("./routes/Users");
 const auth = require("./routes/Auth");
-
-const app = express();
-
+const app = (0, express_1.default)();
 const checkAuthenticated = authenticated.checkAuthenticated;
 const checkNotAuthenticated = authenticated.checkNotAuthenticated;
-
 //require("dotenv").config();
-
 //set view engine to ejs
 app.set("view engine", "ejs");
-
 const port = config.APP_PORT;
-
 if (!config.TODO_JWTPRIVATEKEY) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined");
-  process.exit(1);
+    console.error("FATAL ERROR: jwtPrivateKey is not defined");
+    process.exit(1);
 }
 mongoose.connect(config.DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
-
 app.use(cors()); //enable cors
-
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express_1.default.static(path.join(__dirname, "public")));
 //getUsers();
 app.use(flash());
-app.use(
-  session({
+app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-  })
-);
-
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
 app.use("/api/auth", auth);
 app.use("/api/users", users);
 app.use("/api/todos", todos);
-
 //console.log(users);
-
 app.get("/", (req, res) => {
-  res.send("Welcome to the Front");
+    res.send("Welcome to the Front");
 });
-
 // // get login page
 // app.get('/login', checkNotAuthenticated, (req, res) => {
 //   res.render('pages/login', {});
 // });
-
 // // login to TODOS page
 // app.post(
 //   '/login',
@@ -90,12 +79,10 @@ app.get("/", (req, res) => {
 //     failureFlash: true,
 //   })
 // );
-
 // // get register page
 // app.get('/register', checkNotAuthenticated, (req, res) => {
 //   res.render('pages/register', {});
 // });
-
 // app.post('/register', async (req, res) => {
 //   try {
 //     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -118,43 +105,35 @@ app.get("/", (req, res) => {
 //       .catch((errors) => {
 //         // res.status(500).json({ errors });
 //       });
-
 //     //console.log(req.body);
 //   } catch {
 //     res.redirect('/register');
 //   }
-
 //   //console.log(users);
 // });
-
 // app.get('/logout', (req, res) => {
 //   req.logOut();
 //   //console.log("I'm here");
 //   //req.session.destroy(err=>{})
 //   res.redirect('login');
 // });
-
 // // catch 404 and forward to error handler
 // app.use((req, res, next) => {
 //   next(createError(404));
 // });
-
 // // error handler
 // app.use((err, req, res) => {
 //   // set locals, only providing error in development
 //   res.locals.message = err.message;
 //   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
 //   // render the error page
 //   res.status(err.status || 500);
 //   res.render('error');
 // });
-
 //app.listen(config.APP_PORT); // Listen on port defined in environment
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+    console.log(`Example app listening on port ${port}!`);
 });
-
 // function getUsers() {
 //   userRepository.findAll().then((users) => {
 //     //return users;
@@ -170,5 +149,4 @@ app.listen(port, () => {
 //     );
 //   });
 // }
-
 module.exports = app;
---
+++ src/app.ts
@@ -0,0 +1,84 @@
+///////////////////////////////////////////
+//app.ts
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config;
+}
+// const createError = require("http-errors");
+import express, { Request, Response } from "express";
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+// const initialisePassport = require("./config/passport-config");
+
+// const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+// const userRepository = require("./controllers/userControllers");
+const authenticated = require("./middleware/authenticate");
+const config = require("./config/Config");
+const todos = require("./routes/Todos");
+const users = require("./routes/Users");
+const auth = require("./routes/Auth");
+
+const app = express();
+
+const checkAuthenticated = authenticated.checkAuthenticated;
+const checkNotAuthenticated = authenticated.checkNotAuthenticated;
+
+//require("dotenv").config();
+
+//set view engine to ejs
+app.set("view engine", "ejs");
+
+const port = config.APP_PORT;
+
+if (!config.TODO_JWTPRIVATEKEY) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined");
+  process.exit(1);
+}
+mongoose.connect(config.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors()); //enable cors
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+//getUsers();
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+app.use("/api/auth", auth);
+app.use("/api/users", users);
+app.use("/api/todos", todos);
+
+//console.log(users);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the Front");
+});
+
+// Listen on port defined in environment
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+module.exports = app;
